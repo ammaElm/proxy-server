6d3dae00a3ec912b44662bd902b2710befac2ee2
zhangminyan <zhangminyan@sensetime.com>
2021-12-30 15:56:33 +0800

    all

--- proxy-server.js
+++ proxy-server.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // HOST 指目标地址
 // PORT 服务端口
-const { HOST = 'http://10.151.3.176:9200', PORT = '3300' } = process.env;
+const { HOST = 'http://10.151.217.11:9100', PORT = 3300 } = process.env;
 
 // // 超时时间
 // const TIME_OUT = 30 * 1e3;
@@ -21,10 +21,12 @@ server.use('',express.static('../../sensetime/project2.0/MedViewer/dist'));
 
 // 反向代理（这里把需要进行反代的路径配置到这里即可）
 // eg:将/api/test 代理到 ${HOST}/api/test
-// server.use('/ws/api', createProxyMiddleware({ target: HOST, changeOrigin: true }));
+server.use('/ws', createProxyMiddleware({ target: HOST, changeOrigin: true }));
+server.use('/config', createProxyMiddleware({ target: HOST, changeOrigin: true }));
+// server.use('/wss', createProxyMiddleware({ target: HOST, changeOrigin: true }));
 
 
 // 监听端口
 server.listen(PORT, () => {
-  console.log(`server running ......`);
+  console.log(`server running ......${PORT}`);
 });
